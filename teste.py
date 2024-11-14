1d6041d7fa9e24658bdcc7fd80dd221a3e73de7e
Elisangela Ferreira dos Santos <elisa.fds@gmail.com>
2024-11-14 16:19:55 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,5 +4,5 @@ valor_compra = float(input("Digite o valor da compra: R$ "))
 desconto = 10
 cupom_de_desconto = (nome + "É10")
 
-print(f"Olá, {nome} Em {mes} você fez uma compra no valor de R$ {valor_compra} e ganhou um desconto de {desconto}% em sua próxima compra. Use o cupom {cupom_de_desconto}")
+print(f"Olá, {nome} Em {mes} você fez uma compra no valor de R$ {valor_compra} e ganhou um desconto de {desconto}% em sua próxima compra. Use o cupom {cupom_de_desconto}") 
 
