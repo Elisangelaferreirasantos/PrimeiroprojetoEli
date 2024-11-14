3b309334df7db32612647471dfb6cfff69241c81
kevenjesus01001@gmail.com <kevenJesus01>
2024-11-13 21:09:21 -0300

    valor

---
+++ Operações.py
@@ -0,0 +1,13 @@
+valor = int(input("insira um valor: "))
+
+dobro = valor*2
+triplo = valor*3
+quadrado = valor**2
+raiz_quadrada = valor ** 1/2
+raiz_cubica = valor ** 1/3
+
+print(f"O dobro do {valor} é: {dobro}")
+print(f"O triplo do {valor} é: {triplo}")
+print(f"O quadrado do {valor} é: {quadrado}")
+print(f"A raiz quadrada do {valor} é: {raiz_quadrada}")
+print(f"A raiz cubica do {valor} é: {raiz_cubica}")
