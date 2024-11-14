7c1ef0bb31f282765946a49d9efb640387870ec3
Elisangela Ferreira dos Santos <elisa.fds@gmail.com>
2024-11-14 16:21:14 -0300

    Update Roupa.py

--- Roupa.py
+++ Roupa.py
@@ -1,3 +1,3 @@
 cliente = ("ELI")
-print("Olá " + cliente + " . Em JANEIRO você fez uma compra no valor de R$ 550,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom " + cliente ++ "É10")
+print("Olá " + cliente + " . Em JANEIRO você fez uma compra no valor de R$ 550,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom " + cliente + "É10")
 
