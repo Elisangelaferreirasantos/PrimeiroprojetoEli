c6de61f19a8397df0bc0513072de6c356df37394
Elisangelaferreirasantos <elisa.fds@gmail.com>
2024-11-18 21:17:12 -0300

    funções

---
+++ Dicionario de nomes.py
@@ -0,0 +1,10 @@
+pessoas = {
+    "Ana": 25,
+    "Bruno": 30,
+    "Carlos": 22,
+    "Diana": 28,
+    "Eduardo": 35
+}
+
+for nome, idade in pessoas.items():
+    print(f"Nome: {nome}, Idade: {idade}")
--- Função.py
+++ Função.py
@@ -12,6 +12,51 @@ def add_fruta(a, b):
 
 add_fruta("Limão" , "Morango")
 
-for frutas in lista:
-    print (f"os elementos contidos na lista são: {frutas}")
+var_txt = "texto"
+for letras in var_txt:
+        print (letras)
+
+for x in lista:
+    print (f"Os elementos contidos na lista são: {x}")
+
+    dicionario = {"nome": "Caique" , "idade": 22}
+    for chave in dicionario:
+         print(f"As chaves neste dicionario são: {chave}")
+
+         for w in dicionario.values():
+              print (f"Os valores neste dicionario são: {w}")
+            
+
+dicionario = {"nome": "Caique" , "idade": 22}
+
+for chave,valor in dicionario.items():
+     print(dicionario["idade"])
+     print(dicionario["nome"])
+     print(f"chave: {chave}, valor: {"valor"}")
+
     
+          
+lista1 = ['Banana', 'Maçã', 'Uva']
+
+def add_frutas(a,b):
+     lista1.append(a)
+     lista1.append(b)
+     print(lista1)
+
+add_frutas("Abacte" ,"Sorvete")
+
+def del_frutas():
+         lista1.pop()
+         print(lista1) 
+
+del_frutas()
+
+for x in lista1:
+      print(f"Esta fruta esta na lista {x}")
+
+
+
+
+
+
+
