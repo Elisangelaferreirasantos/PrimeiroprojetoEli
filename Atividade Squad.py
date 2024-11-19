fd329399e1046de047f84a26064596b8d608fcd2
Elisangelaferreirasantos <elisa.fds@gmail.com>
2024-11-19 19:57:36 -0300

    AtividadeSquad

--- Atividade Squad.py
+++ Atividade Squad.py
@@ -1,10 +1,14 @@
 tupla = (1, 2, 3, 4, 5)
+
 lista = list(tupla)
+
 lista.append(6)
 lista.append(7)
+
 lista.pop(0)
 lista.pop(-1)
 print("Primeiro dado da lista:", lista[0])
 print("Quantidade de dados na lista:", len(lista))
-dicionario = {"Nome": "Paulo", "Idade": 40, "Profissão": "GERENTE"}
+
+dicionario = {"Nome": "Eli", "Idade": 45, "Profissão": "TI"}
 print("Nome do dicionário:", dicionario["Nome"])
